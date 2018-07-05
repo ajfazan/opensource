bf9d08e01c1b5b0c526e5ebe38592fcc66bbe637
ajfazan <ajfazan@gmail.com>
2018-07-05 17:46:06 -0300

    Updating repo opensource

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ find -name CMakeFiles -type d | xargs rm -rf
 find \( -name CMakeCache.txt -or -name Makefile -or -name cmake_install.cmake \
                              -or -name install_manifest.txt \) -type f -delete
 
-cd utility
+cd ./utility
 cmake -DCMAKE_BUILD_TYPE=Release .
 make install
 
